bd7261de3b089900d8b47164d6f961b8de77c939
vakopaksadze <Vakopaksadze2014@gmai.com>
2025-03-12 17:28:47 +0400

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,16 @@
+Name = "Nika"
+naMe = "Luka"
+NAME = "Data"
+naME = "Vano"
+name = "Demetre"
+NaMe = "Jan"
+name = "Giorgi"
+# აქ არის 6 ცვლადი
+
+
+my_dog = "charlie"
+my_favourite_color = "black"
+my_favourite_food = "burger"
+my_age = 10
+my_name = "vako"
+print(my_dog,my_favourite_color,my_favourite_food,my_age,my_name)
---
+++ day 08/homework/homework.py
@@ -0,0 +1,9 @@
+# snakecase გამოიყენება რომ ბევრი სიტყვა  გამოვიყენოთ ცვლადში
+
+
+
+
+
+number1 = 10
+number2 = 20
+print(number1 + number2)
