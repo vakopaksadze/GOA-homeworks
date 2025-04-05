230c7b60ca17ed0942b8c04d425363eb6950cbcc
vakopaksadze <Vakopaksadze2014@gmai.com>
2025-04-05 18:13:37 +0400

    commit

---
+++ day 16/classwork/cw.py
@@ -0,0 +1,6 @@
+print(True and False and True or False and True and True or False or False and True)
+#False
+print(False or True and True or False and True or True and False)
+#False
+rint(10 > 5 and True or False and 23 == 23 and False or 100 / 100 == 1 or 5 * 5 == 30 and 10 < 30 > 20)
+True
---
+++ day 16/homework/hw.py
@@ -0,0 +1,56 @@
+# sequencing - კოდის თანმიმდევრობითი (ზემოდან ქვემოთ) გაშვებაSelection - არჩევანის გაკეთება/ რამოდენიმე
+# მოცემული არჩევნიდან გადაწყვეტილების მიღება პროგრამაში 
+# control flow არის კოდის შესრულების ლოგიკა 
+
+
+print("vako")
+print("lika")
+
+age = 11
+if age <= 11: 
+  print("შენ ხარ თინეიჯერი")
+else: 
+  print("შენ არ ხარ თინეიჯერი")
+
+
+password = "123 "
+
+if password =="123":
+   print("პაროლი სწორია")
+else:
+   print("არასწორი პაროლი")
+
+
+
+   password = "11"
+
+if password =="11":
+   print("პაროლი სწორია")
+else:
+   print("არასწორი პაროლი")
+
+
+
+
+
+
+   name = "ვაკო"  # ცვლადში ვინახავთ სახელს
+
+password = input("შეიყვანე პაროლი: ")
+
+if password == "1234":
+    print("password is correct")
+else:
+    print("password is incorrect")
+
+
+
+
+
+
+number = int(input("შეიყვანეთ რიცხვი"))
+
+if number % 2 == 0:
+   print("number is even")
+else:
+   print(numberis odd)
