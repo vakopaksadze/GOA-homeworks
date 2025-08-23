60b9cddea1a89fb408cc22aa43beba895d3253c0
vakopaksadze <Vakopaksadze2014@gmail.com>
2025-08-23 14:19:50 +0400

    commit

---
+++ day 48/homework/hw.py
@@ -0,0 +1,69 @@
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+         return value1 * value2
+    return value1 / value2 
+def check_for_factor(base, factor):
+    return base % factor == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def find_average(numbers):
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)
+a = "code"
+b = "wa.rs"
+name = a + b
+
+
+
+
+
+
+
+
+
+
+
+
+def quarter_of(month):
+    return (month - 1)
+    def say_hello(name):
+    print(f"Hello, {name}")
---
+++ day 52/homework/hw.py
@@ -0,0 +1,7 @@
+def multiply(a,b):
+    return a*b
+
+def simple_multiplication(number):
+    if number % 2==0:
+        return number*8
+    return number*9
